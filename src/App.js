da53a825c1fd1791b08540486d1be7566e147dde
Hızır Kocaman <hizirrkocaman@gmail.com>
2021-07-13 23:29:43 +0300

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const INITIAL_STATE = [
+  { id: 1, baslik: 'HTML Öğren', tamamlandi: true },
+  { id: 2, baslik: 'CSS Öğren', tamamlandi: true },
+  { id: 3, baslik: 'JavaScript Öğren', tamamlandi: true },
+  { id: 4, baslik: 'React Öğren', tamamlandi: false },
+];
+
 function App() {
+  const [liste, setListe] = useState(INITIAL_STATE);
+  const [yeniBaslik, setYeniBaslik] = useState('');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h1>Yapılacaklar Listesi</h1>
+      <div className='ekleme_formu'>
+        <input
+          value={yeniBaslik}
+          onChange={(e) => setYeniBaslik(e.target.value)}
+          placeholder='Listeye Ekle'
+        />
+        <button
+          onClick={() => {
+            setListe([
+              ...liste,
+              { id: Date.now(), baslik: yeniBaslik, tamamlandi: false },
+            ]);
+            setYeniBaslik('');
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Ekle
+        </button>
+      </div>
+      <div className='liste'>
+        {liste.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => {
+              setListe(
+                liste.map((el) =>
+                  el.id === item.id ? { ...el, tamamlandi: !el.tamamlandi } : el
+                )
+              );
+            }}
+            className={item.tamamlandi ? 'yapildi' : ''}
+          >
+            {item.baslik}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() => setListe(liste.filter((item) => !item.tamamlandi))}
+        className='temizle'
+      >
+        Tamamlananları Temizle
+      </button>
     </div>
   );
 }
